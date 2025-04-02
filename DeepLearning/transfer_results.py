7d86ffc5c723bf37d1b5ef3d8557169e5533cefb
Hip02 <hippolyte.hilgers@gmail.com>
2025-04-02 22:30:35 +0200

    Homemade from 9 to 16

--- DeepLearning/transfer_results.py
+++ DeepLearning/transfer_results.py
@@ -10,8 +10,8 @@ local_destinations = os.path.expanduser("/Users/hippolytehilgers/Desktop/UCL_Hip
 
 # --- Entrée utilisateur : nom du sous-dossier à transférer ---
 local_destinations = os.path.join(local_destinations, "ResNet_Homemade")
-local_destinations = [f"{local_destinations}_{i}" for i in [1,2,3,4]]
-subfolders = [f"Regression/ResNet_Homemade_{i}/fold5" for i in [1,2,3,4]]
+local_destinations = [f"{local_destinations}_{i}" for i in [5,6,7,8]]
+subfolders = [f"Regression/ResNet_Homemade_{i}/fold5" for i in [5,6,7,8]]
 
 for local_dest, subfolder in zip(local_destinations, subfolders):
     # --- Construction des chemins distants et locaux ---
