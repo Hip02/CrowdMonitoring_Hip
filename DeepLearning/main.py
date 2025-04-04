c4c519bc64d6c450130e2f50ff06a4346e75c643
Hip02 <hippolyte.hilgers@gmail.com>
2025-04-04 20:04:20 +0200

    add in main and test

--- DeepLearning/main.py
+++ DeepLearning/main.py
@@ -18,7 +18,7 @@ all = [f"NewExp{i}" for i in range(1, 51)]
 
 #l_exp = ["Regression/ResNet_Folds_ExpLR", "Regression/ResNet_Folds_OneCycleLR", "Regression/ResNet_Folds"]
 #l_exp = [f"Regression/ResNet_Folds_debug{i}" for i in range(1, 40)]
-l_exp = [f"Regression/ResNet_Homemade_{i}" for i in range(0, 40)]
+l_exp = [f"Regression/BackToResNet{i}" for i in range(0, 40)]
 
 # Get experiment path from index
 try:
