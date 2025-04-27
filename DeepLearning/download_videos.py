52debb4f7f7ade0d534541178ac24a787c1ca1b6
Hip02 <hippolyte.hilgers@gmail.com>
2025-04-27 12:07:39 +0200

    last videos

---
+++ DeepLearning/download_videos.py
@@ -0,0 +1,10 @@
+import subprocess
+import os
+
+local_path = os.path.expanduser("~/Downloads")  # <-- corrige ici
+
+for i in range(61, 81):
+    print(f"Copying file for exp{i}...")
+    remote_path = f"hhilgers@betelgeuse:/linux/hhilgers/Code/CrowdMonitoring_Hip/DeepLearning/visualizations/fold9_exp{i}_visu.mp4"
+    command = ["scp", "-r", remote_path, local_path]
+    subprocess.run(command)
