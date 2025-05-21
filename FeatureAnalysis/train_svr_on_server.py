8576299e63bc064359affec4d16e9338004bdf8a
Hip02 <hippolyte.hilgers@gmail.com>
2025-05-21 19:09:19 +0200

    fix

--- FeatureAnalysis/train_svr_on_server.py
+++ FeatureAnalysis/train_svr_on_server.py
@@ -8,24 +8,6 @@ base_path = "/linux/hhilgers/Dataset"
 all = [f"NewExp{i}" for i in range(1, 51)]
 data_loader = DataLoader(base_path, exp_list=all)
 
-# Load Labels and Features
-labels = data_loader.get_labels()
-
-features = np.column_stack([
-    data_loader.get_max_values(),
-    data_loader.get_feature("MeanMagnitudes"),
-    data_loader.get_feature("StdMagnitudes"),
-    data_loader.get_feature("MedianMagnitudes"),
-    data_loader.get_feature("SkewnessMagnitudes"),
-    data_loader.get_feature("KurtosisMagnitudes"),
-    data_loader.get_feature("EntropyMagnitudes"),
-    data_loader.get_feature("SpectralEntropyMagnitudes"),
-    data_loader.get_feature("PeakCountMagnitudes"),
-    data_loader.get_feature("ImpulseFactorMagnitudes"),
-    data_loader.get_feature("CrestFactorMagnitudes"),
-    data_loader.get_feature("ClearanceFactorMagnitudes")
-])
-
 from sklearn.svm import SVR
 import itertools
 
