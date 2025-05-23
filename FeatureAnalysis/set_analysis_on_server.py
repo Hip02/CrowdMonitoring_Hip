df69fd1978ed998347b19ef5754947069aca4bf7
Hip02 <hippolyte.hilgers@gmail.com>
2025-05-23 12:04:10 +0200

    debug

--- FeatureAnalysis/set_analysis_on_server.py
+++ FeatureAnalysis/set_analysis_on_server.py
@@ -130,7 +130,7 @@ legend_elements = [
 fig.legend(handles=legend_elements, loc='upper center', bbox_to_anchor=(0.5, 0.92), ncol=5, fontsize=ft_size+2, frameon=False)
 
 # Mise en page finale
-plt.tight_layout(rect=[0, 0, 0.88, 0.84])
+plt.tight_layout(rect=[0, 0, 0.88, 0.78])
 fig.subplots_adjust(top=0.85, right=0.88)
 
 fig.suptitle("Experiments sorted by average number of people (5 Density Categories Highlighted)", fontsize=ft_size+8, y=0.95)
